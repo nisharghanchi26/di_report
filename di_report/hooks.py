801844d2c25516ca7d6b99ca40c3d952cecb80a3
nisharghanchi26 <accit.assist@gmail.com>
2025-07-17 13:06:06 +0000

    added fileds in employee doctype

--- di_report/hooks.py
+++ di_report/hooks.py
@@ -242,3 +242,11 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [
+    {
+        "doctype": "Custom Field",
+        "filters": [
+            ["dt", "=", "Employee"]
+        ]
+    }
+]
